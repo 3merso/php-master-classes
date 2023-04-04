e8ac6cad33e1bbdab89604a36cf03edf63bfef21
Emerson da Silva Martins <emersonsm@icloud.com>
2023-04-04 16:29:42 -0300

    constantes

---
+++ php-basico/constantes.php
@@ -0,0 +1,20 @@
+<?php
+
+// fefinindo constantes
+define('LINK_SITE', 'http://php.net');
+echo LINK_SITE;
+echo '<br>';
+
+// constantes do PHP
+
+echo PHP_VERSION;
+echo '<br>';
+
+echo __DIR__;
+echo '<br>';
+
+echo __FILE__;
+echo '<br>';
+
+echo PHP_OS;
+echo '<br>';
