33b0548197392b6ef2016d8898b09338760a94b2
Emerson da Silva Martins <emersonsm@icloud.com>
2023-04-07 17:41:08 -0300

    operações aritméticas básicas

--- php-basico/operadores/aritmeticos.php
+++ php-basico/operadores/aritmeticos.php
@@ -3,3 +3,22 @@
 $num1 = 10;
 $num2 = 20;
 
+// manipulações básicas
+echo $num + $num2;
+echo '<br>';
+
+echo $num - $num2;
+echo '<br>';
+
+echo $num * $num2;
+echo '<br>';
+
+echo $num / $num2;
+echo '<br>';
+
+echo $num % $num2;
+echo '<br>';
+
+// exponenciação
+echo $num ** $num2;
+echo '<br>';
