b01e2c9422980260dd16ae82a42cb12fd0318ae1
Emerson da Silva Martins <emersonsm@icloud.com>
2023-04-10 17:04:57 -0300

    opeadores de atribuição

---
+++ php-basico/operadores/atribuicao.php
@@ -0,0 +1,22 @@
+<?php
+
+$num = 10;
+
+// valor inicial mais 5
+$num += 5;
+
+// valor inicial menos 5
+$num -= 5;
+
+// valor inicial multiplica por 5
+$num *= 5;
+
+// valor inicial dividido por 5
+$num /= 5;
+
+// valor inicial menos 5
+$num -= 5;
+
+// concatena strings
+$nome = 'John';
+$nome .= ' Sobrenome';
