718ef1ecfc5c9e9bb0e52829ef41e15a65a1224a
Emerson da Silva Martins <emersonsm@icloud.com>
2023-04-13 12:12:29 -0300

    estruturas de repeticao - se, senão

---
+++ php-basico/estruturas-controle/condicionais/se-senao.php
@@ -0,0 +1,15 @@
+<?php
+
+// sintaxe básica
+if (true) {
+    echo "condição satisfeita";
+} else {
+    echo "condição não satisfeita";
+}
+
+// sintaxe alternativa
+if (true) :
+    echo "condição satisfeita";
+else:
+    echo "condição não satisfeita";
+endif;
