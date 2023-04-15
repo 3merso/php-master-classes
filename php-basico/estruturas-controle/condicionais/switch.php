b5f4ceffc977699ce80d1d6a17b82a605c243b59
Emerson da Silva Martins <emersonsm@icloud.com>
2023-04-15 13:25:08 -0300

    estrutura switch

---
+++ php-basico/estruturas-controle/condicionais/switch.php
@@ -0,0 +1,20 @@
+<?php
+
+$fruta = 'Maçã';
+
+switch ($fruta) {
+    case "Banana";
+    echo "A fruta é uma banana";
+    break;
+
+    case "Laranja";
+    echo "A fruta é uma banana";
+    break;
+
+    case "Maçã";
+    echo "A fruta é uma banana";
+    break;
+
+    default:
+    echo "é uma fruta inesperada";
+}
