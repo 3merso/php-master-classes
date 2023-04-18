d20117fd342ad33238d0ab2ea2e8185d241537d6
Emerson da Silva Martins <emersonsm@icloud.com>
2023-04-18 12:32:53 -0300

    iniciando com arrays

---
+++ php-basico/arrays/array.php
@@ -0,0 +1,12 @@
+<?php
+
+$listaFrutas = array('laranja', 'banana', 'uva');
+
+// php 5.4+
+$listaFrutas = ['laranja', 'banana', 'uva'];
+
+// pra printar todo conteúdo
+print_r($listaFrutas);
+
+// pra printar o conteúdo de uma posição
+echo $listaFrutas[0];
