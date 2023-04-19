de5ba16833d6cc436e6824e41c836a3617984207
Emerson da Silva Martins <emersonsm@icloud.com>
2023-04-19 12:12:33 -0300

    arrays associativos

---
+++ php-basico/arrays/array-associativo.php
@@ -0,0 +1,9 @@
+<?php
+
+// colocamos a chave manualmente
+$usuario = [
+    'nome' => 'emerson',
+    'email' => 'e@gmail.com',
+    'idade' => 30
+];
+echo $usuario['nome'] . ' tem a idade de ' . $usuario['idade'] . ' anos';
