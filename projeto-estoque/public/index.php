e419c5ec5688d670976b67d7041e4bef05f3518d
Emerson da Silva Martins <emersonsm@icloud.com>
2023-05-03 12:16:18 -0300

    usando parâmetros de URL

--- projeto-estoque/public/index.php
+++ projeto-estoque/public/index.php
@@ -1,9 +1,13 @@
 <?php
 
 // expor apenas a pasta public: php -S localhost:3030 -t public
-echo 'ok';
 
-$url = '';
+// $_GET; superglobal para pegar parametros de url
+// query string ou query params
+// localhost:3030?nome=emerson
+// localhost:3030?nome=emerson&idade=30
+
+$url = $_GET['url'];
 
 if ($url == '/') {
     echo 'Página inicial';
