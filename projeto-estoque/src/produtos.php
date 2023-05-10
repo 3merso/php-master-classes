19c7afa2b32f643188536178ecc3f706b7155d4e
Emerson da Silva Martins <emersonsm@icloud.com>
2023-05-10 16:46:48 -0300

    criando arquivo com produtos - simulando banco de dados

--- projeto-estoque/src/produtos.php
+++ projeto-estoque/src/produtos.php
@@ -4,6 +4,16 @@ return [
     [
         'codigo' => '1',
         'nome' => 'p1',
-        'preço' => '1.1',
-    ]
-    ];
+        'preco' => '1.1'
+    ],
+    [
+        'codigo' => '2',
+        'nome' => 'p2',
+        'preco' => '1.2'
+    ],
+    [
+        'codigo' => '3',
+        'nome' => 'p3',
+        'preco' => '1.3'
+    ],
+];
