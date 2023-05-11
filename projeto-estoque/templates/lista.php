70faf198eaeeb601a34b5d49a2851f65a63ca446
Emerson da Silva Martins <emersonsm@icloud.com>
2023-05-11 17:01:43 -0300

    mesclando HTML no PHP

--- projeto-estoque/templates/lista.php
+++ projeto-estoque/templates/lista.php
@@ -1,3 +1,14 @@
-Página inicial
+<!-- Página inicial
 
-<?php var_dump(PRODUTOS);
+< ?php var_dump(PRODUTOS); -->
+
+<html>
+    <head>
+        <title>Listagem de produtos</title>
+    </head>
+    <body>
+        <?php foreach (PRODUTOS as $p) {
+            echo $p['nome'] . '-' . $p['preco']; 
+         } ?>
+    </body>
+</html>
