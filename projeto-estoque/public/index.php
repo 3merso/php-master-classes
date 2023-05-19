bea30d1ef997c1e0e89ad9d71435651db5b6c965
Emerson da Silva Martins <emersonsm@icloud.com>
2023-05-19 15:45:39 -0300

    usando array_filter

--- projeto-estoque/public/index.php
+++ projeto-estoque/public/index.php
@@ -10,6 +10,7 @@ require __DIR__ . '/../bootstrap.php';
 $url = isset($_GET['url']) ? $_GET['url'] : '/';
 
 if ($url == '/') {
+    $titulo = 'Listagem de produtos';
     include TEMPLATES . '/lista.phtml'; // apenas inclui, causa warning se não encontrado
     // require '../templates/lista.php'; // se não encontrado, fatal error
 }
@@ -19,13 +20,21 @@ if ($url == '/produto') {
     $codigoProduto = isset($_GET['codigo']) ? $_GET['codigo'] : '';
 
     if ($codigoProduto) {
+        $titulo = 'Detalhes do produto';
         $produtoDetalhe = '';
-        foreach (PRODUTOS as $p) {
-            if ($produto['codigo'] == $codigoProduto) {
-                $produtoDetalhe = $produto;
-                break;
-            }
-        }
+        // foreach (PRODUTOS as $p) {
+        //     if ($produto['codigo'] == $codigoProduto) {
+        //         $produtoDetalhe = $produto;
+        //         break;
+        //     }
+        // }
+
+        // alternativa ao foreach
+        $produtoDetalhe = array_filter(PRODUTOS, function($produto) use ($codigoProduto) {
+            return $produto['codigo'] == $codigoProduto;
+        });
+
+        $produtoDetalhe = current($produtoDetalhe);
     }
 
     if (!$codigoProduto || !isset($produtoDetalhe)) {
