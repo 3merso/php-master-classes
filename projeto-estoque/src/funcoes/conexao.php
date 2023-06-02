e2830ac1fa8fe3f216f5df47fc8554c27140d548
Emerson da Silva Martins <emersonsm@icloud.com>
2023-06-02 17:40:35 -0300

    Criando tabela de produtos no DB

--- projeto-estoque/src/funcoes/conexao.php
+++ projeto-estoque/src/funcoes/conexao.php
@@ -6,3 +6,15 @@ $conexao = new PDO(
     "root", ""
 );
 var_dump($conexao);
+
+// CRIAÇÃO DA TABELA DE PRODUTOS
+// CREATE TABLE produtos(
+//     id INT(11) PRIMARY KEY AUTO_INCREMENT
+//     nome VARCHAR(255) NOT NULL,
+//     descricao VARCHAR(255) NULL,
+//     sobre LONGTEXT,
+//     preco FLOAT(10, 2),
+//     status TINYINT(1),
+//     criado_em DATETIME,
+//     atualizado_em DATETIME,
+// )engine=innoDB;
